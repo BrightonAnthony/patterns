febcb838883aa3fbe93d0209a464c7134945eac9
Brighton <110602910+BrightonAnthony@users.noreply.github.com>
2024-06-14 00:13:09 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -2,6 +2,6 @@ n = int(input("Enter number of lines"))
 for i in range(0,n):
     for j in range(i,n-1):
         print(" ",end=" ")
-    for k in range(0,2*i+1):
+    for k in range(0,2*i+i):
         print("*",end=" ")
     print()
